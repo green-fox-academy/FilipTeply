0f1d7b9425aa4c90da4cd93c2341c9660c35982b
Filip Teply <fteply.ft@gmail.com>
2018-09-20 11:05:44 +0200

    initial exercises

---
+++ week-01/day-4/src/Exercise01.java
@@ -0,0 +1,13 @@
+public class Exercise01 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Looping" + i + "th turn");
+        }
+
+    }
+        System.out.println("End of Loop");
+    }
+
+}
---
+++ week-01/day-4/src/exercise03.java
@@ -0,0 +1,5 @@
+public class exercise03 {
+    public static void main(String[] args) {
+        
+    }
+}

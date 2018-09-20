fe651da38a70d229f107d515325c2f603fcbdb6d
Filip Teply <fteply.ft@gmail.com>
2018-09-20 14:53:39 +0200

    exercises 07-11 added

---
+++ week-01/day-4/src/Example09.java
@@ -0,0 +1,16 @@
+public class Example09 {
+
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        double BMI;
+                BMI = massInKg / Math.pow(heightInM, 2) ;
+        System.out.println("Your Body Mass Index is: " + BMI);
+        // BMI = weight (kg) ÷ height2 (m2)
+
+        // Print the Body mass index (BMI) based on these values
+
+        //25.628077262971846
+    }
+}
---
+++ week-01/day-4/src/Exercise10.java
@@ -0,0 +1,22 @@
+public class Exercise10 {
+
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+
+        // Your name as a string
+        String name = "Filip";
+        System.out.println(name);
+
+        // Your age as an integer
+        int age = 42;
+        System.out.println(age);
+
+        // Your height in meters as a double
+        double height = 1.76;
+        System.out.println(height);
+
+        // Whether you are married or not as a boolean
+        boolean married = true;
+        System.out.println(married);
+    }
+}

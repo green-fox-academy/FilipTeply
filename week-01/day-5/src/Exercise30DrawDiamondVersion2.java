1797933901e5cc1fdf830030aa191e198ecad2e2
Filip Teply <fteply.ft@gmail.com>
2018-09-22 14:02:39 +0200

    ex.30DrawDiamondVersion2

---
+++ week-01/day-5/src/Exercise30DrawDiamondVersion2.java
@@ -0,0 +1,54 @@
+// blueprint:
+// for diamond with height (eg. 5)
+// line number (lineNo) for the 1st line is calculated: height/2 (ie 5/2 = 2)
+// lineNo count down (lineNo--) goes from height/2 to -height/2 (i.e. 2, 1, 0, -1, -2)
+// number of spaces printed on each line before the asterisks = absolute value of lineNo (Math.abs(), ie 2, 1, 0, 1, 2)
+// number of asterisks on each line: (height - (Math.abs(lineNo))*2)
+// absolute value of lineNo
+
+
+import java.util.Scanner;
+import java.lang.*;
+import static java.lang.Math.abs;
+
+
+public class Exercise30DrawDiamondVersion2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me height of your diamond.");
+        int height = scanner.nextInt();
+
+        int lineNo= height/2;
+
+
+//1st loop: loops height times
+for (int n = 0; n < height; n++ ) {
+
+    //1.1 loop prints i spaces i from 0 to absolute value of lineNo
+    for (int i = 0; i < Math.abs(lineNo); i++) {
+        System.out.print(" ");
+    }
+    //1.1 loop fin
+
+
+    //1.2 loop prints asterisks
+    for (int i = 0; i < (height - (Math.abs(lineNo))*2); i++) {
+        System.out.print("*");
+    }
+    //1.2 loop fin
+
+    // lineNo x finishes
+    System.out.println(" ");
+    lineNo--;
+
+  }
+//1st loop fin
+
+
+
+    }
+
+
+}

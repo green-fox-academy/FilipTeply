986fa2561e2654e3900e651ac18617806940746d
Filip Teply <fteply.ft@gmail.com>
2018-09-26 11:43:08 +0200

    exercises up to CenteredSquare

---
+++ week-02/day-3/src/HorizontalLines.java
@@ -0,0 +1,51 @@
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a 50 long horizontal line from that point.
+        // Draw 3 lines with that function. Use loop for that.
+
+        for (int i = 0; i < 3 ; i++) {
+            drawHorizontal(graphics, 100, 100+30*i);
+        }
+    }
+
+public static void drawHorizontal(Graphics gr, int x, int y) {
+
+
+        gr.drawLine(x, y, x+50, y);
+
+
+    }
+
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}

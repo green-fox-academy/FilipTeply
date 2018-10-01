35e349d64b1e98ef97da37039c98da430005cdd3
Filip Teply <fteply.ft@gmail.com>
2018-10-01 22:58:20 +0200

    SymmetricMatrix ArrayList converted to 2D array successfully, checking elements does not work, though

--- week-03/day-1/src/SymmetricMatrixVer2.java
+++ week-03/day-1/src/SymmetricMatrixVer2.java
@@ -23,24 +23,66 @@ public class SymmetricMatrixVer2 {
 
         symmetricMatrix.add(new ArrayList<Integer>(Arrays.asList(1, 0, 1)));
         symmetricMatrix.add(new ArrayList<Integer>(Arrays.asList(0, 2, 2)));
-        symmetricMatrix.add(new ArrayList<Integer>(Arrays.asList(1, 2, 5)));
+        symmetricMatrix.add(new ArrayList<Integer>(Arrays.asList(2, 2, 5)));
 
         System.out.println(isSymmetric(symmetricMatrix)); // should print out true
     }
 
     public static boolean isSymmetric(ArrayList<ArrayList<Integer>> testMatrix) {
-        boolean bool = true;
+        boolean bool = false;
+        int[][] arr = new int[testMatrix.size()][testMatrix.size()];
+
         for (int i = 0; i < testMatrix.size(); i++) {
             for (int j = 0; j < testMatrix.get(i).size(); j++) {
-                if (testMatrix.get(i).get(j) == testMatrix.get(j).get(i)) {
+                arr[i][j] = (testMatrix.get(i).get(j));
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i][j] == arr[j][i]) {
                     bool = true;
+                } else {
+                    bool = false;
                 }
-                else {bool = false;
-                                }
             }
         }
-      return bool;
+        return bool;
     }
-
-
 }
+
+
+//        for(int i=0;i<arr.length;i++){
+//        for(int j=0;j<arr.length;j++){
+//        if(arr[i][j]==arr[j][i]){
+//        bool=true;
+//        }else{
+//        bool=false;
+//        }
+//        }
+//        }
+//        return bool;
+
+
+//        if (arr[i][j] == arr[j][i]) {
+//        bool = true;
+//        } else {
+//        bool = false;
+//        }
+
+//    public static boolean isSymmetric(ArrayList<ArrayList<Integer>> testMatrix) {
+//        boolean bool = false;
+//        for (int i = 0; i < testMatrix.size(); i++) {
+//            for (int j = 0; j < testMatrix.get(i).size(); j++) {
+//                if (testMatrix.get(i).get(j) == testMatrix.get(j).get(i)) {
+//                    bool = true;
+//                }
+//                else if (testMatrix.get(i).get(j) != testMatrix.get(j).get(i)) {
+//                    bool = false;
+//                }
+//            }
+//        }
+//        return bool;
+//    }

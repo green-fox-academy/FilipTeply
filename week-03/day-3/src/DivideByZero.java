5bb315cf903a805fbfb0110f1cbf15f29c885473
Filip Teply <fteply.ft@gmail.com>
2018-10-02 11:21:31 +0200

    Divide by zero with result infinity

---
+++ week-03/day-3/src/DivideByZero.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+
+    // Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double divisor = sc.nextInt();
+
+        divide10ByNumber(divisor);
+
+    }
+
+    public static void divide10ByNumber(double divisor) {
+
+double result =  10.0 / divisor;
+        try {
+            System.out.println(result);
+
+        } catch (ArithmeticException except) {
+            System.out.println("Fail");
+        }
+
+    }
+
+
+}
+
---
+++ week-03/day-3/src/MaterialReview1.java
@@ -0,0 +1,30 @@
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//import java.util.List;
+//
+//public class MaterialReview1 {
+//
+//    public static void main(String[] args) {
+//        Path myPath = Paths.get("instruction.txt");
+//        Path myPath = Paths.get(types.txt);
+//        Files.createFile(types.txt);
+//        File.write(myPath, "Boolean char int byte stream double long short".getBytes());
+//
+//        try {
+//        List<> ourFile = Files.readAllLines(myPath);
+//
+////        for (int i = 0; i < ourFile.size(); i++) {
+////        ourFile.get(i)
+////        }
+//            for (String line: ourFile) {
+//                System.out.println(line);
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}

13bc5ccae44f2c2f5a5d5af0881c2e777b1d0267
Filip Teply <fteply.ft@gmail.com>
2018-10-10 19:59:34 +0200

    Instruments and StringedInstruments...continued

--- week-04/day-2/src/main/java/music/StringedInstrument.java
+++ week-04/day-2/src/main/java/music/StringedInstrument.java
@@ -1,25 +1,25 @@
-package main.java.music;
-
-public abstract class StringedInstrument extends Instrument {
-
-    public int numberOfStrings;
-
-    public StringedInstrument(String name, int numberOfStrings) {
-        super(name);
-        this.numberOfStrings = numberOfStrings;
-    }
-
-    public StringedInstrument(int numberOfStrings) {
-        this();
-        this.numberOfStrings = numberOfStrings;
-    }
-
-    public abstract void sound();
-
-
-//    introduces numberOfStrings and
-//    sound() method what's implementation is yet unknown
-//    but with the help of the sound() the play() method is fully implementable
-
-
-}
+//package main.java.music;
+//
+//public abstract class StringedInstrument extends Instrument {
+//
+//    public int numberOfStrings;
+//
+//    public StringedInstrument(String name, int numberOfStrings) {
+//        super(name);
+//        this.numberOfStrings = numberOfStrings;
+//    }
+//
+//    public StringedInstrument(int numberOfStrings) {
+//        this();
+//        this.numberOfStrings = numberOfStrings;
+//    }
+//
+//    public abstract void sound();
+//
+//
+////    introduces numberOfStrings and
+////    sound() method what's implementation is yet unknown
+////    but with the help of the sound() the play() method is fully implementable
+//
+//
+//}

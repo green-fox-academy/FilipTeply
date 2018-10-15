afcb3b9831b89a7bfa23fef14282a54d1c3f68b0
Filip Teply <fteply.ft@gmail.com>
2018-10-16 00:07:19 +0200

    Converter and Test for three-digit numbers still with tens and teens problems, to be continued

--- week-5/dojo/src/Conveter/Converter.java
+++ week-5/dojo/src/Conveter/Converter.java
@@ -7,6 +7,9 @@ public class Converter {
 
     public static void main(String[] args) {
 
+        //System.out.println(noToWordConverter(321));
+        System.out.println(noToWordConverter(119));
+
 
     }
 
@@ -14,57 +17,75 @@ public class Converter {
 
         ArrayList<String> unitWords = new ArrayList<String>(Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"));
         ArrayList<String> tensWords = new ArrayList<String>(Arrays.asList(null, "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"));
-
+        ArrayList<String> teenWords = new ArrayList<String>(Arrays.asList(null, "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"));
         String stringOut = "";
 
 
         //units in 3-digit integers
         int units;
 
-        //tens in 3-digit integers
+        //teens, tens, hundreds in 3-digit integers
+        int teens;
         int tens;
         int hundreds;
         int numberStrippedFromUnits;
 
         units = number % 10;
+        teens = ((number % 20));
         tens = ((number % 100) - units) / 10;
         hundreds = (number - tens - units) / 100;
 
-        numberStrippedFromUnits = number - units;
+        //System.out.println(number - hundreds * 100);
+        System.out.println(hundreds + " " + tens + " " + teens + " " + units);
 
+        //numberStrippedFromUnits = number - units;
+
+        //hundreds "one hundred" - "nine hundred"
         if (number > 100) {
             for (int i = 0; i < 10; i++) {
                 if (hundreds == i) {
-                    stringOut = stringOut.concat(unitWords.get(i) + " hundred ");
+                    stringOut = stringOut.concat(unitWords.get(i) + " hundred");
                 }
             }
-        } else if (number % 100 != 0) {
+            //gives and when tens and/or units follow hundreds
+        }
+        if (number > 100 && number % 100 != 0) {
             stringOut = stringOut.concat(" and ");
         }
-
-
+        //gives tens "twenty" to "ninety"
         if (number >= 20) {
             for (int i = 0; i < 10; i++) {
                 if (tens == i) {
-                    stringOut = stringOut.concat(tensWords.get(i));
+                    stringOut = stringOut.concat(tensWords.get(i)+ " ");
                 }
             }
-        } else if (number < 20) {
-            stringOut = stringOut.concat(" and ");
-
+        }
+        if (number == 10 || (number - hundreds * 100)==10) {
+            stringOut = stringOut.concat("ten");
         }
 
+        //gives teens "eleven" to "nineteen"
 
+        if ((number < 20 && number > 10 || (number > 100 && (number - hundreds * 100) < 20) && (number - hundreds * 100) > 10) ||
+                ((number > 100 && (number - hundreds * 100) < 20) && (number > 100 && (number - hundreds * 100) > 10))) {
             for (int i = 0; i < 10; i++) {
-                if (units == i) {
-                    stringOut = stringOut.concat(" " + unitWords.get(i));
+                if (teens == i) {
+                    stringOut = stringOut.concat(teenWords.get(i));
                 }
             }
+        }
 
+//gives units "one" to "nine"
+        if (number < 10
+                || (number > 100 && (number - hundreds * 100) < 10)
+                || (number > 100 && (number - hundreds * 100) > 20)) {
+            for (int i = 0; i < 10; i++) {
+                if (units == i) {
+                    stringOut = stringOut.concat(unitWords.get(i));
+                }
+            }
+        }
         return stringOut;
-
-
     }
-
-
 }
+
--- week-5/dojo/src/Conveter/ConverterTest.java
+++ week-5/dojo/src/Conveter/ConverterTest.java
@@ -8,41 +8,151 @@ import static org.junit.Assert.assertEquals;
 public class ConverterTest {
     Converter mainConverter = new Converter();
 
-    @Test
-    public void returnsOneForThreeDigitNo() {
-        int number = 321;
-        assertEquals("one", mainConverter.noToWordConverter(number));
-    }
+//    @Test
+//    public void returnsOneForThreeDigitNo() {
+//        int number = 321;
+//        assertEquals("one", mainConverter.noToWordConverter(number));
+//    }
+//
+//    @Test
+//    public void returns9ForThreeDigitNo() {
+//        int number = 999;
+//        assertEquals("nine", mainConverter.noToWordConverter(number));
+//    }
+//
+//    @Test
+//    public void returnsZeroForThreeDigitNo() {
+//        int number = 320;
+//        assertEquals("zero", mainConverter.noToWordConverter(number));
+//    }
 
     @Test
-    public void returns9ForThreeDigitNo() {
-        int number = 999;
-        assertEquals("nine", mainConverter.noToWordConverter(number));
-    }
-
-    @Test
-    public void returnsZeroForThreeDigitNo() {
-        Converter mainConverter = new Converter();
-
-        int number = 320;
-        assertEquals("zero", mainConverter.noToWordConverter(number));
-    }
-
-    @Test
-    public void returns10sForThreeDigitNo() {
-        Converter mainConverter = new Converter();
-
+    public void returns322For322No() {
         int number = 322;
         assertEquals("three hundred and twenty two", mainConverter.noToWordConverter(number));
     }
 
     @Test
-    public void returns10For10() {
-        Converter mainConverter = new Converter();
+    public void returns11For11() {
+        int number = 11;
+        assertEquals("eleven", mainConverter.noToWordConverter(number));
+    }
 
+    @Test
+    public void returns19For19() {
+        int number = 19;
+        assertEquals("nineteen", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns119For119() {
+        int number = 19;
+        assertEquals("nine hundred and nineteen", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns999For999() {
+
+        int number = 999;
+        assertEquals("nine hundred and ninety nine", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns0For0() {
+        int number = 0;
+        assertEquals("zero", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns20For20() {
+        int number = 20;
+        assertEquals("twenty ", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns90For90() {
+        int number = 90;
+        assertEquals("ninety", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns99For99() {
+        int number = 90;
+        assertEquals("ninety nine", mainConverter.noToWordConverter(number));
+    }
+    @Test
+    public void returns100For100() {
+        int number = 100;
+        assertEquals("one hundred", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns900For900() {
+        int number = 900;
+        assertEquals("nine hundred", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns950For950() {
+        int number = 950;
+        assertEquals("nine hundred and fifty", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns10For10() {
         int number = 10;
         assertEquals("ten", mainConverter.noToWordConverter(number));
     }
+
+    @Test
+    public void returns710For710() {
+        int number = 710;
+        assertEquals("seven hundred and ten ", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns110For110() {
+        int number = 110;
+        assertEquals("one hundred and ten ", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns113For113() {
+        int number = 113;
+        assertEquals("one hundred and thirteen", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns120For120() {
+        int number = 120;
+        assertEquals("one hundred and twenty ", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns50For50() {
+        int number = 50;
+        assertEquals("fifty ", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns930For930() {
+        int number = 930;
+        assertEquals("nine hundred and thirty ", mainConverter.noToWordConverter(number));
+    }
+
+    @Test
+    public void returns990For990() {
+        int number = 990;
+        assertEquals("nine hundred and ninety ", mainConverter.noToWordConverter(number));
+    }
+
+    //tens, teens, make problem,
+    //119,990,930 710,
+    //910
+    // Expected :nine hundred and ninety
+    //Actual   :nine hundred and ninety zero
+
+    //expand for thousands
 
 
 

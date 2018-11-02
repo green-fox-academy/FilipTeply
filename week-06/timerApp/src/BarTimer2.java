527a5aa5147b1aa35e13ef4668a910a7dd3cffaa
Filip Teply <fteply.ft@gmail.com>
2018-11-02 08:17:31 +0100

    BarTimer2 app 0th working version;-)

--- week-06/timerApp/src/BarTimer2.java
+++ week-06/timerApp/src/BarTimer2.java
@@ -16,11 +16,6 @@ public class BarTimer2 {
     public static void mainDraw(Graphics graphics) {
 
 
-        int[] colorGreen = {0, 153, 0};
-        int[] colorBlue = {51, 153, 255};
-        int[] colorOrange = {255, 153, 0};
-        int[] colorRed = {255, 51, 51};
-
         //graphics.fillRect(0, 0, 480, 1005);
 
         // shrinkingBar(graphics, colorGreen, 0, 0, 0);
@@ -33,19 +28,19 @@ public class BarTimer2 {
 //            graphics.fillRect(20, 20, 30, i * 3);
 //        }
 
-        shrinkingBar(graphics, colorGreen, 0, 20, 20);
+        // shrinkingBar(graphics, colorGreen, 0, 0, 0);
 
     }
 
     public static void shrinkingBar(Graphics graphics, int[] color, int i, int posX, int posY) {
         graphics.setColor(new Color(color[0], color[1], color[2]));
-        double height = 180;
+        double height = 900;
         double decrement = height / 60.0;
         i++;
         if (i >= 60) {
             return;
         } else {
-            graphics.fillRect(posX, posY, 20, (int) (height - i * decrement));
+            graphics.fillRect(posX, posY, 480, (int) (height - i * decrement));
             shrinkingBar(graphics, color, i + 1, posX, posY);
         }
 
@@ -58,7 +53,6 @@ public class BarTimer2 {
 
     public static void main(String[] args) {
 
-        //second s = new second();
 
         JFrame jFrame = new JFrame("Drawing");
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -73,32 +67,56 @@ public class BarTimer2 {
 
         //while (sec != 0) {
         //  sec--;
-        panel.repaint();
-        try {
-            //60 * 1000 milliseconds = 1 second
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        panel.repaint();
+//        try {
+//            //60 * 1000 milliseconds = 1 second
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         //}
 
     }
 
     static class ImagePanel extends JPanel implements ActionListener {
 
-        Timer t = new Timer(1000, this);
-        double x = 0, y = 0, velY = 2;
 
+        Timer t = new Timer(100, this);
+        double x = 0;
+        double y1 = 0;
+        double y2 = 0;
+        double y3 = 0;
+        double y4 = 0;
 
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
             mainDraw(graphics);
             t.start();
-           graphics.fillRect((int) x, (int)y, 20, 180-(int)y);
+
+            int[] colorGreen = {0, 153, 0};
+            int[] colorBlue = {51, 153, 255};
+            int[] colorOrange = {255, 153, 0};
+            int[] colorRed = {255, 51, 51};
+
+            graphics.setColor(new Color(colorGreen[0], colorGreen[1], colorGreen[2]));
+            graphics.fillRect((int) x, (int) y1, 480, (int) (1005.0 - y1));
+
+            graphics.setColor(new Color(colorBlue[0], colorBlue[1], colorBlue[2]));
+            graphics.fillRect((int) x + 480, (int) (-1005+y2), 480, (int) (2010.0 - y2));
+
+            graphics.setColor(new Color(colorOrange[0], colorOrange[1], colorOrange[2]));
+            graphics.fillRect((int) x + 960, (int) (-2010+y3), 480, (int) (3015.0 - y3));
+
+            graphics.setColor(new Color(colorRed[0], colorRed[1], colorRed[2]));
+            graphics.fillRect((int) x + 1440, (int)(-3015 + y4), 480, (int) (4020.0 - y4));
+
         }
 
-        public void actionPerformed(ActionEvent e){
-            y+=velY;
+        public void actionPerformed(ActionEvent e) {
+            y1 += 1005.0 / 600.0;
+            y2 += 1005.0 / 600.0;
+            y3 += 1005.0 / 600.0;
+            y4 += 1005.0 / 600.0;
             repaint();
         }
     }
---
+++ week-06/timerApp/src/BarTimer3.java
@@ -0,0 +1,101 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class BarTimer3 {
+
+    public static void mainDraw(Graphics graphics) {
+
+        int[] colorGreen = {0, 153, 0};
+        int[] colorBlue = {51, 153, 255};
+        int[] colorOrange = {255, 153, 0};
+        int[] colorRed = {255, 51, 51};
+
+        shrinkingBar(graphics, colorGreen, 0, 0);
+
+    }
+
+    public static void shrinkingBar(Graphics graphics, int[] color, int posX, int posY) {
+        graphics.setColor(new Color(color[0], color[1], color[2]));
+//        double height = 1005;
+//        double decrement = height / 60.0;
+
+        double y = 0;
+
+        graphics.fillRect(posX, posY, 480, (int) 900.0 - (int) y);
+        //graphics.fillRect(posX, posY, 20, (int) (height - i * decrement));
+        //shrinkingBar(graphics, color, i + 1, posX, posY);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        BarTimer3.ImagePanel panel = new BarTimer3.ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+
+        //int sec = 60;
+
+        //while (sec != 0) {
+        //  sec--;
+//        panel.repaint();
+//        try {
+//            //60 * 1000 milliseconds = 1 second
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        //}
+
+    }
+
+    static class ImagePanel extends JPanel implements ActionListener {
+
+        //        static double height = 1005;
+//        double decrement = height / 60.0;
+//        double x = 0;
+        double y = 0;
+        int[] colorGreen = {0, 153, 0};
+        int[] colorBlue = {51, 153, 255};
+        int[] colorOrange = {255, 153, 0};
+        int[] colorRed = {255, 51, 51};
+
+        Timer t = new Timer(1000, this);
+
+
+
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+            t.start();
+
+
+            //graphics.fillRect(0, (int) 0, 480, 1005 - (int) y);
+            //graphics.fillRect((int) x, (int) y, 20, 180 - (int) y);
+
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            //while (y <= height) {
+            y += 1000.0 / 60.0;
+            repaint();
+            // }
+        }
+    }
+
+
+}
+

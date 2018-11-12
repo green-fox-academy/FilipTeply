1cd5cc5a7242345ec9828ab94c5fedf3d42d5daa
Filip Teply <fteply.ft@gmail.com>
2018-11-12 12:27:07 +0100

    Poker TDD, Card class established, int color, int value, before Hand class introduced

---
+++ week-08/day-1/src/Card.java
@@ -0,0 +1,27 @@
+public class Card {
+
+    int color;
+    int id;
+
+    public Card(int color, int id) {
+        this.color = color;
+        this.id = id;
+    }
+
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
